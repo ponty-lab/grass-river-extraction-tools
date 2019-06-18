7056b2e9ef7b5fb4ca67da3e629bced5301fa5d5
Carla <carlajpont@gmail.com>
2019-06-18 09:02:20 +0100

    Script version 2 - extracting streams from existing stream network

--- src/extract_rivers.sh
+++ src/extract_rivers.sh
@@ -4,14 +4,13 @@
 #      This script extracts river profiles using GRASS GIS tools 
 #      for use with the 2d river inversion model
 #
-#      Carla Pont - 15/June/2019
+#      Carla Pont - 17/June/2019
 # ---------------------------------------------------------------
 
 
 ##### Functions
 
-usage()
-{
+usage() {
    echo ""
    echo "An automated method to extract rivers using GRASS GIS"
    echo "" 
@@ -22,12 +21,14 @@ usage()
    echo "	-f | --dem		Filled DEM to upload"
    echo "	-s | --shapefile	Shapefile to mask DEM"
    echo "	-t | --threshold	minimum catchment size (pixels)"
+   echo "        -n | --number           max number of rivers to extract"
    echo "OPTIONS"
    echo "	-o | --overwrite	Overwrite existing files"
    echo "	-h | --help		help"
    echo ""  
    exit 1
 }
+
 
 ##### Main
 
@@ -53,6 +54,10 @@ while [ "$1" != "" ]; do
          shift
          THRESHOLD=$1
          ;;
+      -n | --number )
+         shift
+         NO_RIVERS=$1
+         ;;
 	  -o | --overwrite )
 	  	 shift
 		 command_args='--overwrite'
@@ -71,7 +76,7 @@ done
 
 #Check if all command line arguments are present
 
-if [[ -z $GRASSDIR || -z $DEM || -z $COAST || -z $THRESHOLD ]]; then
+if [[ -z $GRASSDIR || -z $DEM || -z $COAST || -z $THRESHOLD || -z $NO_RIVERS ]]; then
 	echo "Error: required arguments not set"
 	usage
 	exit 1
@@ -131,17 +136,19 @@ echo ""
 
 r.stream.extract elevation=dem accumulation=facc threshold=$THRESHOLD stream_rast=stream_$THRESHOLD stream_vector=stream_$THRESHOLD direction=fdir $command_args
 
-#Randomly extract points from the DEM using minimum flow accumulation to use as starting points for channel initiation
+#Linking river segments to their downstream portion using 
+#its unique cat number
 
-echo "Extracting random points for stream extraction using THRESHOLD: $THRESHOLD"
-r.mapcalc "facc_$THRESHOLD = if( 'facc' == $THRESHOLD, 1, null())" $command_args
-echo "Exporting X Y coordinates to file: coords.dat"
-r.to.vect input=facc_$THRESHOLD output=facc_$THRESHOLD type=point $command_args
-v.out.ascii in=facc_$THRESHOLD out=coords.dat format=point layer=-1 separator=',' $command_args
+v.db.addcolumn stream_300 columns="x1 double,x2 double,y1 double,y2 double,tostream int"
+v.to.db map=stream_300 option=start columns=x1,y1
+v.to.db map=stream_300 option=end columns=x2,y2
 
-#v.out.ascii in=stream_$THRESHOLD where="stream_type=start" out=coords.dat format=point layer=-1 separator=',' --o
+db.execute sql="create index stream_300_i1 on stream_300(x2,y2)"
+db.execute sql="create index stream_300_i2 on stream_300(x1,y1)"
 
-#Determining cell resolution to calculate drainage area (m^2)
+db.execute sql="update stream_300 set tostream=(select s2.cat from stream_300 s2 where stream_300.x2=s2.x1 and stream_300.y2=s2.y1)"
+
+#Looping through channel heads to extract rivers
 
 eval `g.region -g`
 SQ_M=$( echo "${ewres}*${nsres}" | bc -l )
@@ -150,44 +157,48 @@ echo ""
 echo ""
 echo "Using resolution: $res and cell area: $SQ_M m^2"
 
-#Converting flow direction to degrees
+#Checking number of rivers doesn't exceed number of start_node records
 
-echo ""
-echo "--------------------------------------------"
-echo "Converting flow direction raster to degrees "
-echo "--------------------------------------------"
-echo ""
-r.mapcalc "fdir_deg = if(fdir != 0, 45. * abs(fdir), null())"
+max_rivnum=`db.select -c sql='select COUNT(*) from stream_300 where stream_type="start"'`
+
+if [ "$NO_RIVERS" -lt "${max_rivnum}" ]
+   then
+      lim="$NO_RIVERS"
+   else
+      lim="${max_rivnum}"
+fi
 
 #Loop to extract river profiles. 
-#Output ascii file contains five columns: 
+#Output ascii file contains six columns: 
 #X, Y, cat, length, elev, drainage area (m**2)
 
-i=0
-while read X Y ; do
-   total=`cat coords.dat | wc -l`
-	echo "$X, $Y"
-	i=$(( ${i} + 1))
-   #Determine individual stream path
-	r.drain input=dem direction=fdir_deg output=cpath_$i drain=cpath_$i start_coordinates=$X,$Y --o 
+time for start_node in `db.select -c sql='select cat from stream_300 where stream_type="start" order by random() limit '${lim}''`; do 
+	echo "$start_node"
+   i=$(( ${i} + 1))
+   #Extract linking stream segments
+   v.extract -d input=stream_300 output=line_${i} where="cat in (with recursive cats(cat) as (values(${start_node}) union all select tostream from stream_300 s, cats where s.cat=cats.cat) select cat from cats where cat is not null)" new=0 $command_args
+   #Convert to polyline (to remove duplicate vertices)
+   v.build.polylines input=line_${i} output=cpath_${i} $command_args
    #Extract X, Y coordinates and distance along channel
-	v.to.points input=cpath_$i output=cpathpnt_$i use=vertex dmax=$res layer=-1 --o
+   v.to.points -i input=cpath_${i} output=pnt_${i} use=vertex dmax=${nsres} layer=-1 $command_args
+   #Remove superfluous points for v.build.polylines
+   v.extract input=pnt_${i} where="along > 0" output=cpathpnt_${i} layer=2 $command_args
    #Extract elevation at each point
-	v.what.rast cpathpnt_$i raster=dem column=elev layer=2 --o
+   v.what.rast cpathpnt_$i raster=dem column=elev layer=2 $command_args
    #Extract flow accumulation at each point
-	v.what.rast cpathpnt_$i raster=facc column=accum_pixels layer=2 --o
-	v.db.addcolumn cpathpnt_$i columns="accum_area double" layer=2 --o
+   v.what.rast cpathpnt_$i raster=facc column=accum_pixels layer=2 $command_args
+   v.db.addcolumn cpathpnt_$i columns="accum_area double" layer=2 $command_args
    #Convert flow accumulation pixels to m^2
-	v.db.update cpathpnt_$i column=accum_area query_col="accum_pixels*${SQ_M}" layer=2 --o
+   v.db.update cpathpnt_$i column=accum_area query_col="accum_pixels*${SQ_M}" layer=2 $command_args
    #Drop column with flow accumulation pixels
-	v.db.dropcolumn cpathpnt_$i columns="accum_pixels" layer=2 --o
+   v.db.dropcolumn cpathpnt_$i columns="accum_pixels" layer=2 $command_args
    #Output each stream channel in a separate ascii file
-	v.out.ascii -c input=cpathpnt_$i layer=2 columns=* separator=' ' output=riv$i.dat --o
+   v.out.ascii -c input=cpathpnt_$i layer=2 columns=* separator=' ' output=riv$i.dat $command_args
    echo ""
    echo "----------------------------"
-	echo " River $i of $total created "
+   echo " River $i of ${lim} created "
    echo "----------------------------"
    echo ""
-done < coords.dat
+done
 
 echo "River Extraction Complete!"
